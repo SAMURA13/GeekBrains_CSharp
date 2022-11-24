560f16a9ba4c73929216df1e7a93882a321a571a
Kos10ko <kos10ko.r@yandex.kz>
2022-11-25 02:01:52 +0600

    task 10 done

--- Seminar2/Home_Work_Tasks/task 10/Program.cs
+++ Seminar2/Home_Work_Tasks/task 10/Program.cs
@@ -3,3 +3,18 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<100 || number>1000)
+{
+    Console.Write("Uncorrect number");
+}
+else
+{
+    int rez = (number / 10);
+    rez= rez%10;
+    Console.WriteLine($"Number in midle: {rez}");
+    Console.WriteLine($"Your number: {number}");
+}
