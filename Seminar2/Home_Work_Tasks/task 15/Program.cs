6fdfbd8d7ef006294386fe6c497f959d74d3da5b
Kos10ko <kos10ko.r@yandex.kz>
2022-11-25 01:46:07 +0600

    task 15 done

--- Seminar2/Home_Work_Tasks/task 15/Program.cs
+++ Seminar2/Home_Work_Tasks/task 15/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+// ​
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<=0 || number>=8)
+{
+    Console.Write("Uncorrect number of week");
+}
+    else
+{
+    if (number >=1 && number<= 5)
+{
+    Console.Write("WorkDay");
+}
+    else
+{
+    Console.Write("Weekend!");
+}
+}
