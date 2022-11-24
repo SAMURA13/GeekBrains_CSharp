1673f7f808855a9586dca6e2a438aab39d9810a4
Kos10ko <kos10ko.r@yandex.kz>
2022-11-24 20:45:22 +0600

    task 2 done

--- Seminar1/Home_Work_Tasks/task 2/Program.cs
+++ Seminar1/Home_Work_Tasks/task 2/Program.cs
@@ -3,5 +3,21 @@
 // a = 2 b = 10 -> max = 10, min = 2
 // a = -9 b = -3 -> max = -3, min = -9
 
- Console.WriteLine("Введите число : ");
+ Console.WriteLine("Введите число 1 : ");
  int number = Convert.ToInt32(Console.ReadLine());
+
+  Console.WriteLine("Введите число 2 : ");
+ int number2 = Convert.ToInt32(Console.ReadLine());
+if (number == number2)
+ {
+    break;
+    Console.WriteLine("Они равны");
+ }
+ if (number > number2)
+ {
+    Console.WriteLine("Первое число больше");
+ }
+ else
+ {
+    Console.WriteLine("Второе число больше");
+ }
