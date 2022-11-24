bb6c52a216c9f8072a850e59e82bfda8818d6528
Kos10ko <kos10ko.r@yandex.kz>
2022-11-24 21:47:08 +0600

    task 6 done

--- Seminar1/Home_Work_Tasks/task 6/Program.cs
+++ Seminar1/Home_Work_Tasks/task 6/Program.cs
@@ -4,3 +4,12 @@
 // 7 -> нет
  Console.WriteLine("Введите число : ");
  int number = Convert.ToInt32(Console.ReadLine());
+
+ if ( number % 2 == 0)
+ {
+    Console.Write("Число четное");
+ }
+ else
+ {
+    Console.Write("Число не четное");
+ }
