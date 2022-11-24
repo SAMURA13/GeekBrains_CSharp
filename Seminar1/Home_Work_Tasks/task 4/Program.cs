8e7502ae9f4fe2c59018108f4122e696bb93a51b
Kos10ko <kos10ko.r@yandex.kz>
2022-11-24 21:36:08 +0600

    task 4 done

--- Seminar1/Home_Work_Tasks/task 4/Program.cs
+++ Seminar1/Home_Work_Tasks/task 4/Program.cs
@@ -3,5 +3,25 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
- Console.WriteLine("Введите число : ");
+
+ Console.WriteLine("Введите первое число : ");
  int number = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Введите второе число : ");
+ int number2 = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Введите третье число : ");
+ int number3 = Convert.ToInt32(Console.ReadLine());
+
+ int max = number;
+
+ if (max < number2)
+ {
+    max = number2;
+ }
+ if (max < number3)
+ {
+     max = number3;
+ }
+
+Console.WriteLine($"Самое большое число: {max}");
