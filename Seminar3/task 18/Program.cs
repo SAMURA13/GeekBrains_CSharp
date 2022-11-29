b3673ad5fa1f2fe98b652b7d81275e15f8b6c453
Kos10ko <kos10ko.r@yandex.kz>
2022-11-29 20:33:14 +0600

    task 18 done

--- Seminar3/task 18/Program.cs
+++ Seminar3/task 18/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите четверть: ");
+int diap = Convert.ToInt32(Console.ReadLine());
+
+if (diap==1)
+{
+    Console.WriteLine("Первая четверть x>0 и y>0");
+}
+else if (diap==2)
+{
+    Console.WriteLine("Вторая четверть x<0 и y>0");
+}
+else if (diap==3)
+{
+    Console.WriteLine("Третья четверть x<0 и y<0");
+}
+else if (diap==4)
+{
+    Console.WriteLine("Четвертая четверть x>0 и y<0");
+}
+else if (diap<=0 || diap >=5)
+{
+    Console.WriteLine("Hевозможно найти четверть");
+}
