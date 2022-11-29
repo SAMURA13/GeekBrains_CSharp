eb2a110db1a7cda7f35dcd4075fdd9332a031f4b
Kos10ko <kos10ko.r@yandex.kz>
2022-11-29 20:11:34 +0600

    task 17 done

---
+++ Seminar3/task 17/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №17 Напишите программу которая примимает на вход кординаты (Х и У) причем Х не равен 0 и у не равен 0. 
+// В какой четверти плоскости находится данная точка
+Console.WriteLine("Введите кординату х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кординату у: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x>0 && y>0)
+{
+    Console.WriteLine("1");
+}
+else if (x<0 && y>0)
+{
+    Console.WriteLine("2");
+}
+else if (x<0 && y<0)
+{
+    Console.WriteLine("3");
+}
+else if (x>0 && y<0)
+{
+    Console.WriteLine("4");
+}
