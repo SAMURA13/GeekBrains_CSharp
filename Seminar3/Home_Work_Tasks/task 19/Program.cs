eeaff367a5c33d02ca8ca5c2978251c165f63ca0
Kos10ko <kos10ko.r@yandex.kz>
2022-11-30 22:34:50 +0600

    task 19 change

--- Seminar3/Home_Work_Tasks/task 19/Program.cs
+++ Seminar3/Home_Work_Tasks/task 19/Program.cs
@@ -9,19 +9,16 @@
 Console.WriteLine("Введите : ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number/10000 == number%10 && (number/1000)%10 == (number%100)/10)
-{
-    Console.WriteLine("Да, число является палиндромом");
+int result = 0;
+
+int number1=number; // запоминает число, иначе цикл его затирает
+
+while (number > 0) {
+    result *= 10;
+    result += number % 10;
+    number /= 10;
 }
-else if (number/1000 == number%10 && (number/100)%10 == (number%100)/10)
-{
-    Console.WriteLine("Да, число является палиндромом");
-}
-else if (number/100 == number%10)
-{
-    Console.WriteLine("Да, число является палиндромом");
-}
-else if (number/10 == number%10)
+if (number1==result)
 {
     Console.WriteLine("Да, число является палиндромом");
 }
@@ -30,5 +27,26 @@ else
     Console.WriteLine("Нет, это число не палиндром!");
 }
 
+// Вариант к которму я пришел первый раз
 
+// if (number/10000 == number%10 && (number/1000)%10 == (number%100)/10)
+// {
+//     Console.WriteLine("Да, число является палиндромом");
+// }
+// else if (number/1000 == number%10 && (number/100)%10 == (number%100)/10)
+// {
+//     Console.WriteLine("Да, число является палиндромом");
+// }
+// else if (number/100 == number%10)
+// {
+//     Console.WriteLine("Да, число является палиндромом");
+// }
+// else if (number/10 == number%10)
+// {
+//     Console.WriteLine("Да, число является палиндромом");
+// }
+// else
+// {
+//     Console.WriteLine("Нет, это число не палиндром!");
+// }
 
