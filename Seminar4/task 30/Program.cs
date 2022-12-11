332f6ef11dbdaee409237a37a9fff211f4b109a5
Kos10ko <kos10ko.r@yandex.kz>
2022-12-11 17:40:51 +0600

    V4

--- Seminar4/task 30/Program.cs
+++ Seminar4/task 30/Program.cs
@@ -1,2 +1,31 @@
-﻿int array[]=new test[8];
-Console.WriteLine($"[{string.Join(", ", array)}]");
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array=new int[8];
+
+void FillArray(int[] test)
+{
+    int length = test.Length;
+    int index = 0;
+
+    while (index<length)
+    {
+        test[index] = new Random().Next(1,10);
+    }
+}
+
+void PrintArray(int[] col)
+{
+
+    int count = col.Length;
+    int pos = 0;
+
+    while (pos<count)
+    {
+        Console.WriteLine(col[pos]);
+        pos++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);
