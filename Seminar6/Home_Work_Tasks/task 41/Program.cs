3161e2fef49f291aff10d868004bce6f3025398d
Kos10ko <kos10ko.r@yandex.kz>
2022-12-15 20:46:30 +0600

    try

--- Seminar6/Home_Work_Tasks/task 41/Program.cs
+++ Seminar6/Home_Work_Tasks/task 41/Program.cs
@@ -1,19 +1,18 @@
 ﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
 //  Ввод чисел останавливается при помощи ввода стоп-слова "stop"
 
-    string stop = "stop";
-    string answer="";
+  string stop = "stop";
+    string answer= Console.ReadLine();
     int count=0;
     for (int i = 0;answer != stop; i++)
     {
 
-        int test=int.TryParse(answer);
-        if(test > 0)
+        if(i > 0)
         {
         count++;
         }  
         Console.Write($"Vvedite :  ");
-        answer=Console.ReadLine();      
+        answer=i.ToString(Console.ReadLine());      
     }
     Console.Write($"Вы ввели четных чисел {count-1}");
 
