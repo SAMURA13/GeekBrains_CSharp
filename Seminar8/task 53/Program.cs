06f4d0280c6969df4ceee91af74e7a1640022d88
Kos10ko <kos10ko.r@yandex.kz>
2022-12-21 13:19:11 +0600

    v7

--- Seminar8/task 53/Program.cs
+++ Seminar8/task 53/Program.cs
@@ -1,8 +1,5 @@
-﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
-// m = 3, n = 4.
-// 0 1 2 3
-// 1 2 3 4
-// 2 3 4 5
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
 int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange=1, int rightRange=9)
 {
     int[,] matr = new int[rowsCount, columnsCount];
@@ -12,7 +9,7 @@ int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange=1, int rightRang
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i, j] = i+j;
+            matr[i, j] = rand.Next(leftRange, rightRange + 1);
         }
     }
 
@@ -42,3 +39,24 @@ int rows = GetNumber("Введите число строк");
 int columns = GetNumber("Введите число столбцов");
 int[,] matrix = GetMatrix(rows, columns);
 PrintMatrix(matrix);
+Console.WriteLine();
+
+int Smena(int[,] matr)
+{
+    
+    int temp=0;
+     for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            temp=matr[i,j]-1;
+            matr[i,j]=matr[i,j]-1;
+            temp=matr[i,j];
+            Console.Write(matr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return matr[1,1];
+}
+
+Smena(matrix);
