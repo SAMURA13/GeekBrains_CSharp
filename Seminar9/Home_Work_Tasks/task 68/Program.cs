123aeb2bc40e6e7fb1440e5dc69d4d74f0d28d59
Kos10ko <kos10ko.r@yandex.kz>
2022-12-26 17:44:10 +0600

    v9

---
+++ Seminar9/Home_Work_Tasks/task 68/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int function = Akerman(m, n);
+
+int Akerman(int m, int n)
+{
+  if (m == 0)
+        {
+                return n + 1;
+        } 
+  else if (n == 0)
+        {
+                return Akerman(m - 1, 1);
+        }
+  else
+        {
+                return Akerman(m - 1, Akerman(m, n - 1));
+        }
+
+}
+
+Console.Write($"A(m, n) = {function} ");
+
